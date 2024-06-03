c1eb8cf8cc0aa4648d7515aef7414e6b60a36b47
Jay <51223302+vallemjayanthreddy@users.noreply.github.com>
2024-06-03 14:55:35 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+import openai
+#from dotenv import load_dotenv
+#load_dotenv()
+deployment_name = "test-gpt-jay"
+openai.api_type = "azure"
+#openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key ="d65ae29b824e4b9e84000f40150fae59"
+#openai.api_base = os.getenv("OPENAI_API_BASE")
+openai.api_base = "https://test-azureopenai-jay.openai.azure.com/"
+openai.api_version = "2024-02-01"
+print(os.getenv("OPENAI_API_KEY"))
+prompt =(
+    "Explain what the below SQL query does. Also answer why someone might be interested in this time period, "
+    "and why a company might be interested in this SQL query:\n"
+    "SELECT c.customer_id\n"
+    "FROM Customers c\n"
+    "JOIN Streaming s\n"
+    "ON c.customer_id = s.customer_id\n"
+    "WHERE c.signup_date BETWEEN '2020-03-01' AND '2020-03-31'\n"
+    "AND s.watch_date BETWEEN c.signup_date AND DATE_ADD(c.signup_date, INTERVAL 30 DAY)\n"
+    "GROUP BY c.customer_id\n"
+    "HAVING SUM(s.watch_minutes) > 50 * 60"
+)
+result = openai.Completion.create(
+    prompt=prompt,
+    temperature=0,
+    max_tokens=300,
+    deployment_id=deployment_name
+)
+result.choices[0].text.strip(" \n")
+print(result)
